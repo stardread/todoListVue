7dbf0307f66f86f9fdbd71925d3675391bcf2bac
kbontemps <nesta59440@gmail.com>
2025-03-13 22:43:35 +0100

    chore(files): remove unused files

--- todoListBack/db.ts
+++ todoListBack/db.ts
@@ -1,7 +1,6 @@
 import { DataSource } from "typeorm";
 // in real project it's in .env
 const MONGO_URI='mongodb://localhost:27017/todoList'
-const PORT=5000
 export const createDataSource = () => {
   return new DataSource({
     type: "mongodb",
