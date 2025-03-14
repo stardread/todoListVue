dcd43c28bebfd47cbeb93d027bae72b3a5ce6440
kbontemps <nesta59440@gmail.com>
2025-03-14 21:14:14 +0100

    test(editTask): add edit task test

--- todoListFront/cypress/e2e/main.cy.ts
+++ todoListFront/cypress/e2e/main.cy.ts
@@ -103,7 +103,7 @@ describe('App mounted', () => {
     cy.intercept('DELETE', BASE_TASK_URL + '2156251752', {
       body: {
         acknowledged: true,
-        deletedId: '67d428559c64683af75e609c',
+        deletedId: '2156251752',
       },
     })
 
@@ -118,5 +118,80 @@ describe('App mounted', () => {
     cy.contains('Confirmed').should('be.visible')
     cy.contains('Wash my car').should('not.exist')
     cy.get('[data-testid="task"]').should('have.length', 6)
+  })
+
+  it('Edit task', () => {
+    cy.intercept('GET', BASE_TASK_URL, (req) => {
+      if (callCount === 0) {
+        req.reply({
+          statusCode: 200,
+          body: [
+            {
+              id: '2156251752',
+              title: 'Wash my car',
+              description: 'Wash my car with soap and water',
+              status: 'todo',
+            },
+            ...taskResponse,
+          ],
+        })
+      } else {
+        req.reply({
+          statusCode: 200,
+          body: [
+            {
+              id: '2156251752',
+              title: 'Wash my bike and my car',
+              description: 'Wash my bike and my car with soap and water',
+              status: 'todo',
+            },
+            ...taskResponse,
+          ],
+        })
+      }
+      callCount++
+    })
+
+    cy.intercept('PATCH', BASE_TASK_URL, {
+      body: [
+        {
+          acknowledged: true,
+          modifiedCount: 1,
+          upsertedId: null,
+          upsertedCount: 0,
+          matchedCount: 1,
+        },
+      ],
+    })
+
+    cy.visit('/')
+    cy.get('[data-testid="task"]').should('have.length', 7)
+    cy.get('[data-testid="task"]').first().click()
+    cy.get('[data-testid="edit-task-button"]').first().click()
+    cy.contains('Edit task').should('be.visible')
+
+    cy.get('[data-testid="task-title"]').should('have.value', 'Wash my car')
+    cy.get('[data-testid="task-description"]').should(
+      'have.value',
+      'Wash my car with soap and water',
+    )
+
+    cy.get('[data-testid="task-title"]').clear({ force: true })
+    cy.get('[data-testid="task-title"]').type('Wash my bike and my car', {
+      force: true,
+    })
+
+    cy.get('[data-testid="task-description"]').clear({ force: true })
+    cy.get('[data-testid="task-description"]').type('Wash my bike and my car with soap and water', {
+      force: true,
+    })
+
+    cy.contains('button', 'Save').click()
+    cy.contains('Edit task').should('not.exist')
+    cy.contains('Confirmed').should('be.visible')
+    cy.contains('Wash my car').should('not.exist')
+    cy.contains('Wash my bike and my car').should('be.visible')
+
+    cy.get('[data-testid="task"]').should('have.length', 7)
   })
 })
